b2ea2d8778acee4af18b0b82c16dd202c43561d9
129s <3057653788@qq.com>
2025-04-21 23:50:13 +0800

    2

--- include/midi.h
+++
@@ -1,9 +0,0 @@
-// include/midi.h
-#pragma once
-#include <windows.h>
-#include "mixer.h"
-
-typedef void (*MIDI_Callback)(Mixer *, BYTE, BYTE, BYTE);
-
-int midi_init(MIDI_Callback callback, Mixer *mixer);
-void midi_close();
